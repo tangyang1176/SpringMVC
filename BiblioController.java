735c036a715ca1fe1a7b82d6822b75a2e4038281
tangyang1176 <tangyang1176@gmail.com>
2017-03-27 22:22:19 -0400

    Update BiblioController.java

--- BiblioController.java
+++ BiblioController.java
@@ -13,4 +13,18 @@ public class BiblioController {
            return "biblio";
     }
     
+    @RequestMapping("/addEntry")
+    public String addEntry(@RequestParam(value="author", required=true) String a, 
+    @RequestParam(value="title", required=true) String t,
+    @RequestParam(value="year", required=true) String y,
+    @RequestParam(value="journal", required=true) String j,Model model) {
+         model.addAttribute("author", a);
+         model.addAttribute("title", t);
+         model.addAttribute("year", y);
+         model.addAttribute("journal", j);
+         System.out.println("adding new entry with author "+a);
+         System.out.println("adding new entry with title "+t);
+         System.out.println("adding new entry with year "+y);
+         System.out.println("adding new entry with jornal "+j);
+         return "biblio"; /    
 }
